e16f7404568409d94d1371f493149489fa4c98bb
Povilas Korop <povilask007@gmail.com>
2019-12-26 13:38:51 +0200

    Fixed individual controllers

--- app/Http/Controllers/Admin/ReportsController.php
+++ app/Http/Controllers/Admin/ReportsController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
@@ -9,97 +10,118 @@ class ReportsController extends Controller
 {
     public function fans()
     {
-        $reportTitle = 'Fans';
-        $reportLabel = 'SUM';
-        $chartType   = 'line';
+        $reports = [
+            [
+                'reportTitle' => 'Fans',
+                'reportLabel' => 'SUM',
+                'chartType'   => 'line',
 
-        $results = Engagement::get()->sortBy('stats_date')->groupBy(function ($entry) {
-            if ($entry->stats_date instanceof \Carbon\Carbon) {
-                return \Carbon\Carbon::parse($entry->stats_date)->format('Y-m-d');
-            }
+                'results' => Engagement::get()->sortBy('stats_date')->groupBy(function ($entry) {
+                    if ($entry->stats_date instanceof \Carbon\Carbon) {
+                        return \Carbon\Carbon::parse($entry->stats_date)->format('Y-m-d');
+                    }
 
-            return \Carbon\Carbon::createFromFormat(config('app.date_format'), $entry->stats_date)->format('Y-m-d');
-        })->map(function ($entries, $group) {
-            return $entries->sum('fans');
-        });
+                    return \Carbon\Carbon::createFromFormat(config('app.date_format'),
+                        $entry->stats_date)->format('Y-m-d');
+                })->map(function ($entries, $group) {
+                    return $entries->sum('fans');
+                }),
+            ],
+        ];
 
-        return view('admin.reports', compact('reportTitle', 'results', 'chartType', 'reportLabel'));
+        return view('admin.reports', compact('reports'));
     }
 
     public function engagements()
     {
-        $reportTitle = 'Engagements';
-        $reportLabel = 'SUM';
-        $chartType   = 'line';
+        $reports = [
+            [
+                'reportTitle' => 'Engagements',
+                'reportLabel' => 'SUM',
+                'chartType'   => 'line',
+                'results'     => Engagement::get()->sortBy('stats_date')->groupBy(function ($entry) {
+                    if ($entry->stats_date instanceof \Carbon\Carbon) {
+                        return \Carbon\Carbon::parse($entry->stats_date)->format('Y-m-d');
+                    }
 
-        $results = Engagement::get()->sortBy('stats_date')->groupBy(function ($entry) {
-            if ($entry->stats_date instanceof \Carbon\Carbon) {
-                return \Carbon\Carbon::parse($entry->stats_date)->format('Y-m-d');
-            }
+                    return \Carbon\Carbon::createFromFormat(config('app.date_format'),
+                        $entry->stats_date)->format('Y-m-d');
+                })->map(function ($entries, $group) {
+                    return $entries->sum('engagements');
+                }),
+            ],
+        ];
 
-            return \Carbon\Carbon::createFromFormat(config('app.date_format'), $entry->stats_date)->format('Y-m-d');
-        })->map(function ($entries, $group) {
-            return $entries->sum('engagements');
-        });
-
-        return view('admin.reports', compact('reportTitle', 'results', 'chartType', 'reportLabel'));
+        return view('admin.reports', compact('reports'));
     }
 
     public function reactions()
     {
-        $reportTitle = 'Reactions';
-        $reportLabel = 'SUM';
-        $chartType   = 'line';
+        $reports = [
+            [
+                'reportTitle' => 'Reactions',
+                'reportLabel' => 'SUM',
+                'chartType'   => 'line',
+                'results'     => Engagement::get()->sortBy('stats_date')->groupBy(function ($entry) {
+                    if ($entry->stats_date instanceof \Carbon\Carbon) {
+                        return \Carbon\Carbon::parse($entry->stats_date)->format('Y-m-d');
+                    }
 
-        $results = Engagement::get()->sortBy('stats_date')->groupBy(function ($entry) {
-            if ($entry->stats_date instanceof \Carbon\Carbon) {
-                return \Carbon\Carbon::parse($entry->stats_date)->format('Y-m-d');
-            }
+                    return \Carbon\Carbon::createFromFormat(config('app.date_format'),
+                        $entry->stats_date)->format('Y-m-d');
+                })->map(function ($entries, $group) {
+                    return $entries->sum('reactions');
+                }),
+            ],
+        ];
 
-            return \Carbon\Carbon::createFromFormat(config('app.date_format'), $entry->stats_date)->format('Y-m-d');
-        })->map(function ($entries, $group) {
-            return $entries->sum('reactions');
-        });
-
-        return view('admin.reports', compact('reportTitle', 'results', 'chartType', 'reportLabel'));
+        return view('admin.reports', compact('reports'));
     }
 
     public function comments()
     {
-        $reportTitle = 'Comments';
-        $reportLabel = 'SUM';
-        $chartType   = 'line';
+        $reports = [
+            [
+                'reportTitle' => 'Comments',
+                'reportLabel' => 'SUM',
+                'chartType'   => 'line',
+                'results'     => Engagement::get()->sortBy('stats_date')->groupBy(function ($entry) {
+                    if ($entry->stats_date instanceof \Carbon\Carbon) {
+                        return \Carbon\Carbon::parse($entry->stats_date)->format('Y-m-d');
+                    }
 
-        $results = Engagement::get()->sortBy('stats_date')->groupBy(function ($entry) {
-            if ($entry->stats_date instanceof \Carbon\Carbon) {
-                return \Carbon\Carbon::parse($entry->stats_date)->format('Y-m-d');
-            }
+                    return \Carbon\Carbon::createFromFormat(config('app.date_format'),
+                        $entry->stats_date)->format('Y-m-d');
+                })->map(function ($entries, $group) {
+                    return $entries->sum('comments');
+                }),
+            ],
+        ];
 
-            return \Carbon\Carbon::createFromFormat(config('app.date_format'), $entry->stats_date)->format('Y-m-d');
-        })->map(function ($entries, $group) {
-            return $entries->sum('comments');
-        });
-
-        return view('admin.reports', compact('reportTitle', 'results', 'chartType', 'reportLabel'));
+        return view('admin.reports', compact('reports'));
     }
 
     public function shares()
     {
-        $reportTitle = 'Shares';
-        $reportLabel = 'SUM';
-        $chartType   = 'line';
+        $reports = [
+            [
+                'reportTitle' => 'Shares',
+                'reportLabel' => 'SUM',
+                'chartType'   => 'line',
+                'results'     => Engagement::get()->sortBy('stats_date')->groupBy(function ($entry) {
+                    if ($entry->stats_date instanceof \Carbon\Carbon) {
+                        return \Carbon\Carbon::parse($entry->stats_date)->format('Y-m-d');
+                    }
 
-        $results = Engagement::get()->sortBy('stats_date')->groupBy(function ($entry) {
-            if ($entry->stats_date instanceof \Carbon\Carbon) {
-                return \Carbon\Carbon::parse($entry->stats_date)->format('Y-m-d');
-            }
+                    return \Carbon\Carbon::createFromFormat(config('app.date_format'),
+                        $entry->stats_date)->format('Y-m-d');
+                })->map(function ($entries, $group) {
+                    return $entries->sum('shares');
+                }),
+            ],
+        ];
 
-            return \Carbon\Carbon::createFromFormat(config('app.date_format'), $entry->stats_date)->format('Y-m-d');
-        })->map(function ($entries, $group) {
-            return $entries->sum('shares');
-        });
-
-        return view('admin.reports', compact('reportTitle', 'results', 'chartType', 'reportLabel'));
+        return view('admin.reports', compact('reports'));
     }
 
 }
